ce1f1bbf03a7a1a71f16ab3a64487ac040233226
Mariano <salguero.ms@gmail.com>
2022-07-03 12:33:20 -0300

    port

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
 const getEvents = require('./pageController');
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 5000
 
 app.get('/', async(req, res) => {
+    res.send("Hola!");
+})
+
+app.get('/getEvents', async(req, res) => {
     res.send(await getEvents(295, 310));
 })
 
