ca5abdbe50c794f543dab9d1e4eb5937c886aa0d
Mariano <salguero.ms@gmail.com>
2022-07-06 10:50:23 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const getEvents = require('./pageController');
 const express = require('express')
+const getAllEvents = require('./allEvents')
+const fs = require('fs');
 
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,7 +10,7 @@ app.get('/', async(req, res) => {
     res.send("Hola!");
 })
 
-app.get('/getEvents', async(req, res) => {
+app.get('/getEvent', async(req, res) => {
     let url = 'https://itecno.com.ar/cckirchner/index.asp?event='.concat(req.query.id)
 
     try {
@@ -21,6 +23,21 @@ app.get('/getEvents', async(req, res) => {
     }
 })
 
+app.get('/getAllEvents', async(req, res) => {
+    try {
+        if (fs.existsSync('db.json')) {
+            let data = fs.readFileSync('db.json');
+
+            res.status(200).send(JSON.parse(data));
+        }
+    } catch (ex) {
+        console.log(ex);
+        res.status(400).send(ex)
+    }
+})
+
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
 })
+
+getAllEvents();
