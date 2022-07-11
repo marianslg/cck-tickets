ad3351c2b052d4ca7cf366d421740c71e4ad7a61
Mariano <salguero.ms@gmail.com>
2022-07-11 10:04:34 -0300

    .

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var path = require('path');
 const app = express()
 const port = process.env.PORT || 5000
 
+process.env.TZ = "America/Argentina/Buenos_Aires"
+
 app.get('/', async(req, res) => {
     res.send("Hola!")
 })
@@ -16,6 +18,14 @@ app.get('/getAllEvents', async(req, res) => {
         const data = dbController.getEvents()
 
         res.status(200).send(data)
+    } catch (ex) {
+        res.status(400).send(ex)
+    }
+})
+
+app.get('/getDateTime', async(req, res) => {
+    try {
+        res.status(200).send(moment().format('DD/MM/YYYY hh:mm:ss a'))
     } catch (ex) {
         res.status(400).send(ex)
     }
