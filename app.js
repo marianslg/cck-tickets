c1d500726f00812ca85ffb4cbd3a199ecd6b9dfc
Mariano <salguero.ms@gmail.com>
2022-07-15 08:52:15 -0300

    .

--- app.js
+++ app.js
@@ -2,17 +2,23 @@ const express = require('express')
 var moment = require('moment');
 const scrapping = require('./workers/scrapping')
 const dbController = require('./db/dbController')
+var cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 5000
 
 process.env.TZ = "America/Argentina/Buenos_Aires"
 
-app.get('/', async(req, res) => {
+var corsOptions = {
+    origin: 'https://marianslg.github.io/cck-tickets-front',
+    optionsSuccessStatus: 200
+}
+
+app.get('/', cors(corsOptions), async(req, res) => {
     res.send("Hola!")
 })
 
-app.get('/getAllEvents', async(req, res) => {
+app.get('/getAllEvents', cors(corsOptions), async(req, res) => {
     try {
         const data = dbController.getEvents()
 
@@ -22,7 +28,7 @@ app.get('/getAllEvents', async(req, res) => {
     }
 })
 
-app.get('/getDateTime', async(req, res) => {
+app.get('/getDateTime', cors(corsOptions), async(req, res) => {
     try {
         res.status(200).send(moment().format('DD/MM/YYYY hh:mm:ss a'))
     } catch (ex) {
